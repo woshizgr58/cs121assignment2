9f4e061a54db599ebf2d763088ae1408b9bd0a23
woshizgr58 <kzhong0508@gmail.com>
2026-04-29 15:21:39 -0700

    fix server registration

--- utils/server_registration.py
+++ utils/server_registration.py
@@ -9,6 +9,7 @@ def init(df, user_agent, fresh):
         df.add_one(Register, reg)
         df.commit()
         df.push_await()
+    print("Waiting for cache server assignment...", flush=True)
     while not reg.load_balancer:
         df.pull_await()
         if reg.invalid:
@@ -20,7 +21,12 @@ def init(df, user_agent, fresh):
     return reg.load_balancer
 
 def get_cache_server(config, restart):
+    print(
+        f"Registering with cache coordinator {config.host}:{config.port}...",
+        flush=True)
     init_node = Node(
         init, Types=[Register], dataframe=(config.host, config.port))
-    return init_node.start(
+    cache_server = init_node.start(
         config.user_agent, restart or not os.path.exists(config.save_file))
+    print(f"Using cache server {cache_server[0]}:{cache_server[1]}.", flush=True)
+    return cache_server
